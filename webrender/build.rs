3e92da1c431f4d2204a1de371e14b303c8004065
Attila Dusnoki <dati91@gmail.com>
2018-03-06 13:38:12 +0100

    update travis

--- webrender/build.rs
+++ webrender/build.rs
@@ -608,7 +608,11 @@ fn compile_glsl_to_spirv(file_name_vector: Vec<String>, out_dir: &str) ->  HashM
         file_name.push_str(".spv");
         let spirv_file_path = Path::new(&out_dir).join(&file_name);
         #[cfg(any(target_os = "android", target_os = "linux"))]
-        let mut glslang_cmd = Command::new(Path::new("./tools/glslangValidator"));
+        let mut glslang_validator = String::from_utf8(Command::new("find").arg("../").arg("-name").arg("glslang_validator").output().unwrap().stdout).unwrap();
+        #[cfg(any(target_os = "android", target_os = "linux"))]
+        glslang_validator.pop(); // remove \n
+        #[cfg(any(target_os = "android", target_os = "linux"))]
+        let mut glslang_cmd = Command::new(Path::new(&glslang_validator));
         #[cfg(target_os = "windows")]
         let mut glslang_cmd = Command::new(Path::new("./tools/glslangValidator.exe"));
         glslang_cmd
